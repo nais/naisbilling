6137503befef386b0997d4a940b65fd3018faa6b
vebjorre <vebjorn.rekkebo@nav.no>
2024-11-12 11:03:42 +0100

    use org-folder instead of labels

--- views/tenant_billing/tenant_cost_by_invoice_month.sql
+++ views/tenant_billing/tenant_cost_by_invoice_month.sql
@@ -1,54 +1,33 @@
-with
-  all_tenants as (
-    SELECT
-      invoice.month,
-      (
-        SELECT
-          value
-        from
-          UNNEST (project.labels)
-        WHERE
-          key = 'tenant'
-      ) as tenant_unfiltered,
-      (
-        SUM(CAST(cost AS NUMERIC)) + SUM(
-          IFNULL(
-            (
-              SELECT
-                SUM(CAST(c.amount AS NUMERIC))
-              FROM
-                UNNEST (credits) AS c
-            ),
-            0
-          )
-        )
-      ) AS total_exact
-    FROM
-      `nais-io.nais_billing_regional.gcp_billing_export_resource_v1_014686_D32BB4_68DF8E`
-    GROUP BY
-      1,
-      2
-    ORDER BY
-      1 ASC
-  )
 SELECT
-  month,
+  invoice.month as invoice_month,
   case
-    when tenant_unfiltered is null
-    or tenant_unfiltered in (
-      'nav',
-      'nais',
-      'dev-nais',
-      'ci-nais',
-      'test-nais',
-      'example',
-      'testing'
-    ) then 'nav'
-    else tenant_unfiltered
-  end as tenant,
-  sum(CAST(total_exact AS NUMERIC)) as total_cost
+    when substr(invoice.month, 5, 6) in ('11', '12') then cast(
+      cast(substr(invoice.month, 1, 4) as integer) + 1 as string
+    )
+    else substr(invoice.month, 1, 4)
+  end as billing_year, -- billing year goes from november to october
+  ancestor.display_name as organization,
+  (
+    SUM(CAST(cost AS NUMERIC)) + SUM(
+      IFNULL(
+        (
+          SELECT
+            SUM(CAST(c.amount AS NUMERIC))
+          FROM
+            UNNEST (credits) AS c
+        ),
+        0
+      )
+    )
+  ) AS total_exact
 FROM
-  all_tenants
+  `nais-io.nais_billing_regional.gcp_billing_export_resource_v1_014686_D32BB4_68DF8E`,
+  UNNEST (project.ancestors) as ancestor
+WHERE
+  starts_with(ancestor.resource_name, 'organization')
 GROUP BY
-  month,
-  tenant
+  1,
+  2,
+  3
+ORDER BY
+  1 ASC
