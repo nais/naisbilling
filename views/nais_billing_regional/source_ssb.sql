a60b55800c10bd2c27718cdfc11ba6911d78821c
vebjorre <vebjorn.rekkebo@nav.no>
2024-12-09 09:25:01 +0100

    add partion_date and filter on project.ancestors instead of labels

--- views/nais_billing_regional/source_ssb.sql
+++ views/nais_billing_regional/source_ssb.sql
@@ -1,3 +1,15 @@
-SELECT *
-FROM `nais-io.nais_billing_regional.gcp_billing_export_resource_v1_014686_D32BB4_68DF8E` 
-where (SELECT value from UNNEST(project.labels) WHERE key='tenant') = 'ssb'
+SELECT
+    *,
+    _PARTITIONDATE as partion_date
+FROM
+    `nais-io.nais_billing_regional.gcp_billing_export_resource_v1_014686_D32BB4_68DF8E`
+where
+    EXISTS (
+        SELECT
+            1
+        FROM
+            UNNEST (project.ancestors) as a
+        WHERE
+            STARTS_WITH(a.resource_name, 'organizations/')
+            AND a.display_name = 'ssb.no'
+    )
